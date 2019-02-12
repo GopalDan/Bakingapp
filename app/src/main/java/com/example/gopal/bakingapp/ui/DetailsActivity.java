27a2fc34b5e00d39420d8ebeed10bf11785fd31d
Gopal Dan <gopal125dan@gmail.com>
2019-02-13 02:42:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gopal/bakingapp/ui/DetailsActivity.java
@@ -0,0 +1,119 @@
+package com.example.gopal.bakingapp.ui;
+
+/**
+ * Created by Gopal on 2/12/2019.
+ */
+
+
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.gopal.bakingapp.R;
+import com.example.gopal.bakingapp.Recipe;
+import com.example.gopal.bakingapp.RecipeAdapter;
+import com.example.gopal.bakingapp.Step;
+import com.example.gopal.bakingapp.networking.CustomLoader;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Recipe> {
+    private final String TAG = DetailsActivity.class.getSimpleName();
+    private List<List<String>> ingredients;
+    private List<List<String>> steps;
+    private List<Step> instructions;
+    private int mSelectedId;
+    private RecipeAdapter mAdapter;
+    private final String mUrl = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+    private final int LOADER_ID = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        mSelectedId = getIntent().getIntExtra(MainActivity.CARD_VIEW_ID, 1);
+        instructions = new ArrayList<>();
+
+        mAdapter = new RecipeAdapter(this,new ArrayList<Step>());
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(mAdapter);
+        // when a list item is clicked go to InstructionActivity for showing video & brief description
+        // Using Serializable to pass the list of custom object i.e. List<Step>
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Intent intent = new Intent(DetailsActivity.this, InstructionActivity.class);
+                intent.putExtra("list",(Serializable) instructions);
+                intent.putExtra("position",position);
+                startActivity(intent);
+            }
+        });
+
+        // start the loader
+        getLoaderManager().initLoader(LOADER_ID,null,this);
+    }
+
+    /**
+     * Showing ingredients & steps of selected Recipe in MainActivity
+     * @param singleRecipe The Recipe that is selected
+     */
+    public void showIngredient(Recipe singleRecipe) {
+        //Log.e(TAG, "ID is" + singleRecipe.getId());
+        if (singleRecipe != null) {
+            ingredients = singleRecipe.getIngredients();
+            steps = singleRecipe.getSteps();
+
+            for(int j=0;j<steps.size();j++){
+                List<String> singleInstruction = steps.get(j);
+                String stepNumber = singleInstruction.get(0);
+                String stepInfo = singleInstruction.get(1);
+                String description = singleInstruction.get(2);
+                String videoUrl = singleInstruction.get(3);
+                String thumbnailUrl = singleInstruction.get(4);
+
+                instructions.add(new Step(stepNumber,stepInfo,description, videoUrl, thumbnailUrl));
+            }
+
+            TextView txtView = findViewById(R.id.ingredient_tv);
+            int length = ingredients.size();
+            Log.e(TAG, "Number of ingredients are: " + length);
+
+            for (int i = 0; i <length; i++) {
+                List<String> singleIngredient = ingredients.get(i);
+                String quantity = singleIngredient.get(0);
+                String measure = singleIngredient.get(1);
+                String ingredient = singleIngredient.get(2);
+                String mixUp = getString(R.string.filled_bullet) + ingredient +"(" + quantity + " " +  measure + ")";
+                txtView.append(mixUp);
+                txtView.append("\n");
+            }
+        }
+    }
+
+    @Override
+    public android.content.Loader<Recipe> onCreateLoader(int i, Bundle bundle) {
+        return new CustomLoader(this, mUrl, mSelectedId);
+    }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<Recipe> loader, Recipe recipe) {
+        mAdapter.clear();
+        showIngredient(recipe);
+        mAdapter.addAll(instructions);
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Recipe> loader) {
+
+    }
+}
---
+++ app/src/main/java/com/example/gopal/bakingapp/ui/InstructionActivity.java
@@ -0,0 +1,137 @@
+package com.example.gopal.bakingapp.ui;
+
+/**
+ * Created by Gopal on 2/12/2019.
+ */
+
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.gopal.bakingapp.R;
+import com.example.gopal.bakingapp.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.util.List;
+
+public class InstructionActivity extends AppCompatActivity {
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+    private List<Step> instructions;
+    private int position;
+    private Button mNextStepButton;
+    private Button mPrevStepButton;
+    private TextView mInfo;
+    private int mTotalStep;
+    boolean wasPlaying = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+
+        mInfo = findViewById(R.id.step_info);
+        mPlayerView = (SimpleExoPlayerView) findViewById(R.id.video_player);
+        mNextStepButton = findViewById(R.id.next_step_btn);
+        mPrevStepButton = findViewById(R.id.previous_step_btn);
+
+        // Getting list of custom object(i.e. List<Step> ) using Serializable & position that is clicked
+        instructions = (List<Step>) getIntent().getSerializableExtra("list");
+        position = getIntent().getIntExtra("position",1);
+        mTotalStep = instructions.size()-1;
+        showCurrentStep(position);
+
+        // when next button is clicked
+        mNextStepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( position < mTotalStep ){position = position + 1;}
+                else {position = 0;}
+                showCurrentStep(position);
+            }
+        });
+
+        // when previous button is clicked
+        mPrevStepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(position>0) position = position - 1;
+                else position = mTotalStep;
+                showCurrentStep(position);
+            }
+        });
+
+    }
+
+    public void showCurrentStep(int position){
+        Step currentStep = instructions.get(position);
+        String stepNumber = currentStep.getStepNumber();
+        String description = currentStep.getStepDescription();
+        String videoUrl = currentStep.getStepVideoUrl();
+
+        setTitle("Step " + stepNumber);
+        mInfo.setText(description);
+
+        // if currently video is playing then release it before initialising the others
+          if( wasPlaying ) {
+              wasPlaying = false;
+              releasePlayer();}
+
+        // Initialize the player.
+        if(!TextUtils.isEmpty(videoUrl)){
+            mPlayerView.setVisibility(View.VISIBLE);
+            wasPlaying = true;
+            initializePlayer(Uri.parse(videoUrl));}
+            else{
+            mPlayerView.setVisibility(View.GONE);
+        }
+
+    }
+
+    /**
+     * Initialize ExoPlayer.
+     * @param mediaUri The URI of the sample to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(this, "ClassicalMusicQuiz");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    this, userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+
+}
+
